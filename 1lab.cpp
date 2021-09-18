5d52c24c992e046f5000763d8e25845d28169d33
LMini562 <71384352+lmini562@users.noreply.github.com>
2021-09-18 12:27:12 +0300

    Update 1lab.cpp

--- 1lab.cpp
+++ 1lab.cpp
@@ -3,122 +3,157 @@
 
 using namespace std;
 
-int N = 10; //размер массива
+int N = 10;
 
 class Stack {
 private:
-	int* arr;
-	int top;
+	int* arr; //указатель на массив
+	int top; //переменная для вершины стека
 
 public:
-	Stack() {
+	Stack() //конструктор 
+	{
 		arr = new int[N];
 		top = -1;
 	}
-	Stack(int m) {
+
+	Stack(int m) //конструктор с параметром
+	{
 		arr = new int[m];
 		top = -1;
 	}
-	void Push(int n) {
-		if (top == N - 1) {
-			cout << "Стек переполнен\n";
+
+	void Push(int n) //добавляем
+	{
+		if (top == N - 1) //если верхний эл. равен длинне
+		{
+			cout << "Стек переполнен!" << endl; //то стек переполнен
 		}
-		else {
-			top++;
-			arr[top] = n;
+		else //иначе
+		{
+			top++; //двигаем вверх на 1
+			arr[top] = n; //полученное значение на место верхнего
 		}
 	}
-	int Pop() {
-		if (top < 0) {
-			cout << "Так, у нас стек пуст\n";
+
+	int Pop() //удаляем
+	{
+		if (top < 0) //если верхний элемент <0
+		{
+			cout << "Стек пуст!" << endl; //стек пуст
 			return 0;
 		}
-		else {
-			int element = arr[top];
-			top = -1;
-			return element;
+		else //иначе
+		{
+			int element = arr[top]; //извлекаем значение
+			top -= 1; //уменьшаем значение вершины
+			return element; 
 		}
 	}
 };
+
 class Queue {
 private:
-	int* arr;
-	int head;
-	int tail;
+	int* arr; //указатель на массив
+	int head; //переменная начала очереди
+	int tail; // переменная для конца очереди
 
 public:
-	Queue() {
-		arr = new int[N];
+	Queue() //конструктор без аргументов
+	{
+		arr = new int[N]; 
 		head = 0;
 		tail = 0;
 	}
-	Queue(int n) {
+
+	Queue(int n) //конструктор с 1 аргументом
+	{
 		arr = new int[n];
 		head = 0;
 		tail = 0;
 	}
-	void Put(int newElement) {
-		if (head == (tail + 1) % N) {
-			cout << "Йоу, у нас очередь переполнена\n";
+
+	void Put(int newElement) //добавляем
+	{
+		if (head == (tail + 1) % N) //если в очереди нет места
+		{
+			cout << "Очередь переполнена!" << endl; //то очередь переполнена
 		}
-		else {
-			arr[tail] = newElement;
-			tail = (tail + 1) % N;
+		else //иначе
+		{
+			arr[tail] = newElement; //записываем значение в массив
+			tail = (tail + 1) % N; //даем хвосту номер последнего эл.
 		}
 	}
-	int Get() {
-		if (head != tail) {
-			int output = arr[head];
-			head = (head + 1) % N;
+
+	int Get() //извлекаем
+	{
+		if (head != tail) //проверяем наличие элементов в очереди
+		{
+			int output = arr[head]; //выводим первый с головы эл.
+			head = (head + 1) % N; //присваеваем голове номер след.эл.
 			return output;
 		}
-		else {
-			cout << "Очередь пустая!\n";
+		else //иначе
+		{
+			cout << "Очередь пуста!" << endl;
 			return 0;
 		}
 	}
 };
+
 int main() {
-	int newElement;
-	bool a;
-
+	//русификация
 	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251); 
+	SetConsoleOutputCP(1251);
 
-	cout << "Сколько элементов будет в очереди и стеке?: ";
+	int newElement; //число для ввода
+	bool r; //флаг проверки условий
+
+	cout << "Сколько элементов в очереди и стеке?: ";
 	cin >> N;
 	Stack stack(N);
 	Queue queue(N);
 
-	for (int i = 0; i < N + 5; i++) {
+	for (int i = 0; i < N + 5; i++)
+	{
 		cout << "Чтобы добавить введите 1, чтобы удалить 0: ";
-		cin >> a;
+		cin >> r;
 
-		if (a) {
+		if (r)
+		{
 			cout << "Введите значение: ";
 			cin >> newElement;
-			stack.Push(newElement);
+			stack.Push(newElement); //добавляем
 		}
-		else {
-			int output = stack.Pop();
-			cout << "Значение: \n" << output;
-			cout << endl;
-		}
-	}
-	for (int i = 0; i < N + 5; i++) {
-		cout << "Чтобы добавить введите 1, чтобы удалить 0: ";
-		cin >> a;
-
-		if (a) {
-			cout << "Введите значение: ";
-			cin >> newElement;
-			queue.Put(newElement);
-		}
-		else {
-			int output = queue.Get();
+		else
+		{
+			int output = stack.Pop(); //убираем
 			cout << "Значение: " << output;
 			cout << endl;
 		}
+
 	}
+
+	for (int i = 0; i < N + 5; i++)
+	{
+		cout << "Чтобы добавить введите 1, чтобы удалить 0: ";
+		cin >> r;
+
+		if (r)
+		{
+			cout << "Введите значение: ";
+			cin >> newElement;
+			queue.Put(newElement); //добавляем
+		}
+		else
+		{
+			int output = queue.Get(); //убираем
+			cout << "Значение: " << output;
+			cout << endl;
+		}
+
+	}
+
 	return 0;
 }
